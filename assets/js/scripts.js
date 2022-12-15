a6474e5677623e5b1663ad25c49a711dd9ef211d
Umair Arshad <mumairtipu@gmail.com>
2022-12-16 02:39:12 +0500

    some animation modification

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,23 @@
 $(document).ready(function() {
-	
-	// $('.main-content').scroll3D();
-
-	// $('.s3d__navigation').remove();
 
 	$(".project").hover3d({
 		selector: ".project__card",
 		// shine: true,
 		sensitivity: 4,
 	});
+
+	setInterval(() => {
+		$(".navbutton--next").click()
+	}, 4000);
 	
+});
+
+$(window).on('resize scroll', function() {
+	$('.heading').each((el, index) => {
+		if ($(index).css("opacity") !== '0') {
+			$(index).css("z-index", "2")
+		} else {
+			$(index).css("z-index", "1")
+		}
+	})
 });
